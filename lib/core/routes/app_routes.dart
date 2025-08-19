bb8da9bbd6cdbdeb79ff4e55b16d4505fc359eb8
lhj4499 <ucs210212@naver.com>
2025-08-19 17:11:52 +0900

    feat: 휴대폰 인증 및 회원가입 라우팅 추가 #8

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 import '../../login/screens/login.dart';
 import '../../login/screens/email_login_screen.dart';
 import '../../main.dart';
+import '../../signup/screens/signup_phone_verification.dart';
+import '../../signup/screens/signup_form.dart';
 
 class AppRoutes {
   static const String login = '/';
   static const String home = '/home';
   static const String emailLogin = '/email-login';
+  static const String signUp = '/sign-up';
+  static const String signUpForm = '/sign-up/form';
 
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -25,6 +29,18 @@ class AppRoutes {
       case emailLogin:
         return MaterialPageRoute(
           builder: (context) => const EmailLoginScreen(),
+          settings: settings,
+        );
+
+      case signUp:
+        return MaterialPageRoute(
+          builder: (context) => const SignupPhoneVerificationScreen(),
+          settings: settings,
+        );
+
+      case signUpForm:
+        return MaterialPageRoute(
+          builder: (context) => const SignupFormScreen(),
           settings: settings,
         );
 
