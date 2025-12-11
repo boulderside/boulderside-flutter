397de9a63faf034e9007f28bf2e1ef80641bffdd
이재훈 <potential1205@naver.com>
2025-12-11 18:42:19 +0900

    fix: 로그인, 회원가입, 닉네임 중복 확인 API에 대해 토큰 여부 확인하지 않도록 수정 (#52)

--- lib/src/core/api/api_client.dart
+++ lib/src/core/api/api_client.dart
@@ -25,7 +25,7 @@ class ApiClient {
         onRequest: (o, h) async {
           final tokenStore = GetIt.I<TokenStore>();
           final accessToken = await tokenStore.getAccessToken();
-          final isAuthRequest = o.path.contains('/login') || o.path.contains('/signup');
+          final isAuthRequest = o.path.contains('/auth/oauth/login') || o.path.contains('/auth/oauth/signup') || o.path.contains('/users/nickname/availability');
           if (!isAuthRequest && accessToken != null && o.headers['Authorization'] == null) {
             o.headers['Authorization'] = 'Bearer $accessToken';
           }
