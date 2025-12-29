63b6a672b134fac61fce68051e80760a01653cda
이재훈 <potential1205@naver.com>
2025-12-30 01:14:11 +0900

    feat: apple oauth 로그인 화면 구현, 로그인 API 연동 - ios only (#90)

--- lib/src/features/login/providers/login_providers.dart
+++ lib/src/features/login/providers/login_providers.dart
@@ -1,6 +1,7 @@
 import 'package:boulderside_flutter/src/app/di/dependencies.dart';
-import 'package:boulderside_flutter/src/features/login/application/kakao_login_client.dart';
+import 'package:boulderside_flutter/src/features/login/application/apple_login_client.dart';
 import 'package:boulderside_flutter/src/features/login/application/google_login_client.dart';
+import 'package:boulderside_flutter/src/features/login/application/kakao_login_client.dart';
 import 'package:boulderside_flutter/src/features/login/application/login_view_model.dart';
 import 'package:boulderside_flutter/src/features/login/domain/repositories/auth_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -17,11 +18,16 @@ final googleLoginClientProvider = Provider<GoogleLoginClient>((ref) {
   return GoogleLoginClientImpl();
 });
 
+final appleLoginClientProvider = Provider<AppleLoginClient>((ref) {
+  return AppleLoginClientImpl();
+});
+
 final loginViewModelProvider =
     StateNotifierProvider<LoginViewModel, LoginState>((ref) {
       return LoginViewModel(
         ref.watch(kakaoLoginClientProvider),
         ref.watch(googleLoginClientProvider),
+        ref.watch(appleLoginClientProvider),
         ref.watch(authRepositoryProvider),
       );
     });
